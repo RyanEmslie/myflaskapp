f447767e1b306d23a97a4f396d1a2725cd8c242c
ryanemslie <ryan.emslie@gmail.com>
2019-06-17 18:24:42 -0400

    initial routes setup

---
+++ data.py
@@ -0,0 +1,32 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad nihil eum at klaslk asldkj awein alskjaslj aeliae a;slri a;isejail;er',
+            'author': 'Ryan Emslie',
+            'created_date': '04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad nihil eum at klaslk asldkj awein alskjaslj aeliae a;slri a;isejail;er',
+            'author': 'Ryan Emslie',
+            'created_date': '04-29-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad nihil eum at klaslk asldkj awein alskjaslj aeliae a;slri a;isejail;er',
+            'author': 'Ryan Emslie',
+            'created_date': '05-25-2017'
+        },
+        {
+            'id': 4,
+            'title': 'Article Four',
+            'body': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad nihil eum at klaslk asldkj awein alskjaslj aeliae a;slri a;isejail;er',
+            'author': 'Ryan Emslie',
+            'created_date': '06-15-2017'
+        }
+    ]
+    return articles
