788c40962a847c71a60f2eb0f0553cddd73b8e0d
ryanemslie <ryan.emslie@gmail.com>
2019-06-19 19:23:53 -0400

    4_Database and added an About Page

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,9 @@ from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
+# test for about page
+testData = {'username': 'Ryan'}
+
 
 @app.route('/')
 @app.route('/index')
@@ -29,42 +32,7 @@ def login():
         return redirect(url_for('index'))
     return render_template('login.html',  title='Sign In', form=form)
 
-# from flask import render_template
-# from app import app
-# from app.forms import LoginForm
-# # from data import Articles
 
-
-# @app.route('/')
-# @app.route('/index')
-# def index():
-#     return render_template('base.html')
-# # Articles = Articles()
-# @app.route('/login')
-# def login():
-#     form = LoginForm()
-#     return render_template('login.html', title='Sign In', form=form)
-
-
-# # @app.route('/')
-# # def index():
-# #     return render_template('home.html')
-
-
-# # @app.route('/login')
-# # def login():
-# #     form = LoginForm()
-# #     return render_template('login.html')
-# # @app.route('/about')
-# # def about():
-# #     return render_template('about.html')
-
-
-# # @app.route('/articles')
-# # def articles():
-# #     return render_template('articles.html', articles=Articles)
-
-
-# # @app.route('/article/<string:id>/')
-# # def article(id):
-# #     return render_template('article.html', id=id)
+@app.route('/about')
+def about():
+    return render_template('about.html', user=testData)
